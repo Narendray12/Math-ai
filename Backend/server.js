65670f58d20f5cc69827f692de46e7865df57e38
Narendray12 <narendraydv12@gmail.com>
2024-11-05 14:36:13 +0530

    vercel20

--- Backend/server.js
+++ Backend/server.js
@@ -105,10 +105,31 @@ const app = express();
 app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+} 
 
 const analyzer = new ImageAnalyzer(process.env.GEMINI_KEY);
 
-app.post('/calculate', async (req, res) => {
+app.post('/calculate',allowCors(handler), async (req, res) => {
     try {
       const { image, dict_of_vars } = req.body;
   
